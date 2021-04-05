b5e7d75479a5847fc4c06cdceb8c1390497b6115
TheLastColonial <alex.aim88@gmail.com>
2021-04-05 15:24:11 +0100

    Added SafeCallServiceLogMetadataUnitTests

---
+++ SerilogExperiments.Tests.Unit/Commands/SafeCallServiceLogMetadataUnitTests.cs
@@ -0,0 +1,21 @@
+﻿namespace SerilogExperiments.Tests.Unit.Commands
+{
+    using System;
+    using NUnit.Framework;
+    using SerilogExperiments.Commands;
+
+    /// <summary>
+    /// Unit Tests for <see cref="SafeCallServiceLogMetadata"/>
+    /// </summary>
+    [TestFixture]
+    public class SafeCallServiceLogMetadataUnitTests
+    {
+        public SafeCallServiceLogMetadata Metadata { get; set; }
+
+        [Test]
+        public void Constuct_Successfully()
+        {
+            this.Metadata = new SafeCallServiceLogMetadata(Guid.NewGuid(), typeof(SafeCallServiceLogMetadataUnitTests), "Unit Tests");
+        }
+    }
+}
