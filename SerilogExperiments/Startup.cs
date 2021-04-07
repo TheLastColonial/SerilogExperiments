f0c5615ca753ce0d83d01f1bc3c405c0337ccb5c
TheLastColonial <alex.aim88@gmail.com>
2021-04-07 18:57:06 +0100

    Added swagger route prefix

--- SerilogExperiments/Startup.cs
+++ SerilogExperiments/Startup.cs
@@ -53,7 +53,11 @@ namespace SerilogExperiments
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Serilog Experiments v1"));
+                app.UseSwaggerUI(c => 
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Serilog Experiments v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
